659262c62a468ab36a27db34d1b553280efa6c69
ylighgh <59725600+ylighgh@users.noreply.github.com>
2022-11-07 10:36:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@
 
 import requests
 from happy_python import HappyLog
-from config import UserList, index_url, headers, check_in_url
+from config import UserList, login_url, headers, check_in_url
 
 hlog = HappyLog.get_instance()
 
 
 def check_in(username: str, passwd: str) -> None:
     session = requests.session()
-    session.post(index_url, headers=headers, data={'email': username, 'passwd': passwd})
+    session.post(login_url, headers=headers, data={'email': username, 'passwd': passwd})
     resp = session.post(check_in_url)
     if resp.status_code == 200:
         hlog.info(f"用户{username}签到成功！")
